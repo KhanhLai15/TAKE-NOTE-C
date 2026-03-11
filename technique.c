47bf9272e5ae2872be73a4edc635130db8bbac38
Khánh Lại <06khanhlai@gmail.com>
2026-03-11 16:07:15 +0700

    create file technique

---
+++ technique.c
@@ -0,0 +1,10 @@
+MỘT SỐ KĨ THUẬT LẬP TRÌNH
+
+1. Dùng biến cờ (flag) để kiểm tra điều kiện hoặc trạng thái của chương trình. Biến cờ thường được sử dụng để xác định xem một điều kiện đã được thỏa mãn
+    hay chưa, hoặc để kiểm soát luồng thực thi của chương trình.
+
+2. Sử dụng con trỏ để quản lý bộ nhớ động và truyền dữ liệu giữa các hàm. Con trỏ cho phép bạn làm việc với địa chỉ của biến
+    và có thể giúp tối ưu hóa hiệu suất của chương trình.
+
+3. Sử dụng biến left và right để duyệt một mảng hoặc một chuỗi. Biến left thường được sử dụng để bắt đầu từ đầu mảng, trong khi biến right
+    được sử dụng để bắt đầu từ cuối mảng. Kỹ thuật này thường được sử dụng trong các thuật toán sắp xếp hoặc tìm kiếm.
